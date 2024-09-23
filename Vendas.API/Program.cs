ae1a3e51e147b87301db119962e4892a2d71e32f
Fabio Osorio <f.osorio@outlook.com.br>
2024-09-23 20:49:25 -0300

    chore(API): adicionado as configurações

--- Vendas.API/Program.cs
+++ Vendas.API/Program.cs
@@ -5,6 +5,9 @@ using Microsoft.EntityFrameworkCore;
 using Vendas.Data;
 using Vendas.Data.Interfaces;
 using Vendas.Data.Repositories;
+using Serilog;
+using Microsoft.OpenApi.Models;
+using Vendas.API;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,14 +20,29 @@ builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
 builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
 builder.Services.AddScoped<IVendaService, VendaService>();
 
-builder.Services.AddControllers();
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "123Vendas API",
+        Version = "v1"
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
