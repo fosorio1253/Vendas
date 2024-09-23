b1b180ff27aab26a0fca0b897feb7ce387df8c43
Fabio Osorio <f.osorio@outlook.com.br>
2024-09-23 20:18:31 -0300

    feat(Data): Adicionado conexão com a base de dados, contexto e repositories

--- Vendas.API/Program.cs
+++ Vendas.API/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Vendas.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<VendasContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
