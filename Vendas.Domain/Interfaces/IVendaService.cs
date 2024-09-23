f0074e5f1ab118bad87aa4eff2e29711b52863ab
Fabio Osorio <f.osorio@outlook.com.br>
2024-09-23 18:58:26 -0300

    feat(Domain): Adicionado interface para venda

---
+++ Vendas.Domain/Interfaces/IVendaService.cs
@@ -0,0 +1,13 @@
+﻿using Vendas.DTO;
+
+namespace Vendas.Domain.Interfaces
+{
+    public interface IVendaService
+    {
+        Task<Venda> CriarVendaAsync(Venda venda);
+        Task<Venda> AtualizarVendaAsync(Guid id, Venda venda);
+        Task<bool> CancelarVendaAsync(Guid id);
+        Task<Venda> ObterVendaAsync(Guid id);
+        Task<IEnumerable<Venda>> ObterTodasVendasAsync();
+    }
+}
