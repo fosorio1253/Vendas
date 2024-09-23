d7a59399da32b00af966379e8440b79904e7b8d0
Fabio Osorio <f.osorio@outlook.com.br>
2024-09-23 20:26:37 -0300

    feat(API): Adicionado serviços de repositório

--- Vendas.API/Program.cs
+++ Vendas.API/Program.cs
@@ -1,11 +1,17 @@
 using Microsoft.EntityFrameworkCore;
 using Vendas.Data;
+using Vendas.Data.Interfaces;
+using Vendas.Data.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<VendasContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IVendaRepository, VendaRepository>();
+builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
+builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
